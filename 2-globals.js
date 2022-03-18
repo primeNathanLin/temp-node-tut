d5ad25b91a88b2b9d66733e91aff8dc9c3af5b1c
primeNathanLin <primenathan@hotmail.com>
2022-03-18 19:31:55 +0800

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,14 @@
+// GLOBALS - NO WINDW!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+console.log(__dirname);
+// console.log(process);
+
+setInterval(() => {
+  console.log("Hello World");
+}, 1000);
