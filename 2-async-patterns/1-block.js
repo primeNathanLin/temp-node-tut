b8a1f15bdc886a41ed9810f56bff5b39fc09b595
primeNathanLin <primenathan@hotmail.com>
2022-03-21 15:03:53 +0800

    second commit

---
+++ 2-async-patterns/1-block.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Home Page");
+  } else if (req.url === "/about") {
+    // BLOCKING CODE !!!
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; j < 1000; j++) {
+        console.log(`${i} ${j}`);
+      }
+    }
+    res.end("About Page");
+  } else {
+    res.end("End page");
+  }
+});
+
+server.listen(5000, () => {
+  console.log("Sever is listening on port 5000 ...");
+});
